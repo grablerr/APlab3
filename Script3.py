ee15145ee736a13af88176aad9c14950ec9f0423
Nikita <ppdbavyss@vk.com>
2023-11-27 21:46:17 +0400

    rewrite scripts 1, 2 and 3

--- Script3.py
+++ Script3.py
@@ -1,27 +1,22 @@
+from email import generator
 import random
 import os
 import shutil
-import csv
-from typing import Optional
-
 from Script2 import create_dir
+import csv
 
-
-def get_element(class_name: str) -> Optional[str]:
-    """This function return us list of names in dataset class"""
-    for file_name in os.listdir(os.path.join("dataset", class_name)):
+def get_element(dataset: str, class_name: str) -> generator:
+    for file_name in os.listdir(os.path.join(dataset, class_name)):
         yield file_name
 
-
-def create_randomname_file(annotation_name: str, dir_copy: str) -> None:
-    """This function create the copy of dataset in another directory with names which are random numbers"""
+def create_randomname_file(dataset: str, annotation_name: str, dir_copy: str) -> None:
     file_number = list(range(10001))
     random.shuffle(file_number)
     counter = 1
     create_dir(dir_copy)
-    for dataset_class in os.listdir("dataset"):
-        for file_name in get_element(dataset_class):
-            shutil.copy(os.path.join(os.path.join("dataset", dataset_class), file_name),
+    for dataset_class in os.listdir(os.path.join(dataset)):
+        for file_name in get_element(dataset, dataset_class):
+            shutil.copy(os.path.join(os.path.join(dataset, dataset_class), file_name),
                         os.path.join(dir_copy, f"{file_number[counter]}.txt"))
 
             with open(os.path.join(dir_copy, annotation_name), mode="a", newline='') as file:
@@ -29,6 +24,5 @@ def create_randomname_file(annotation_name: str, dir_copy: str) -> None:
                 file_writer.writerow([f"{file_number[counter]}.txt", dataset_class])
             counter += 1
 
-
-def run3(annotation_name: str, dir_copy: str) -> None:
-    create_randomname_file(annotation_name, dir_copy)
+def run3(dataset: str, annotation_name: str, dir_copy: str) -> None:
+    create_randomname_file(dataset, annotation_name, dir_copy)
